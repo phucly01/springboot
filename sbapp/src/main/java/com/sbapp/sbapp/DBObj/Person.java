909de108847082da65478e36ecdea3c6a57ecf1e
phucly01 <phongthuypts@yahoo.com>
2024-03-13 21:50:18 -0400

    - Set hibernate auto update - Saved db to local file for persistency - Fixed output of http.GET in DbDemoController

--- sbapp/src/main/java/com/sbapp/sbapp/DBObj/Person.java
+++ sbapp/src/main/java/com/sbapp/sbapp/DBObj/Person.java
@@ -13,5 +13,39 @@ public class Person
     private Integer id;
     private String first_name;
     private String last_name;
+    private Integer age;
     
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return this.first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return this.last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public Integer getAge() {
+        return this.age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
 }
--- sbapp/src/main/java/com/sbapp/sbapp/controllers/DbDemoController.java
+++ sbapp/src/main/java/com/sbapp/sbapp/controllers/DbDemoController.java
@@ -1,30 +1,33 @@
 package com.sbapp.sbapp.controllers;
 
 import java.util.Map;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 // import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 // import org.thymeleaf.TemplateEngine;
 // import org.thymeleaf.spring6.view.ThymeleafView;
 // import org.thymeleaf.templateresolver.StringTemplateResolver;
-
 import com.sbapp.sbapp.DBObj.Person;
 import com.sbapp.sbapp.DBServices.RepoService;
 
-@Controller
+@RestController
 public class DbDemoController {
     
     @Autowired
     RepoService serv;
 
     @GetMapping("/db")
-    public String dbview(Map<String, Object> model)
+    public List<Person> dbview()
     {
-        model.put("db", serv.list());
-        return "db";
+        return serv.list();
     }
 
     @PostMapping("/db1")
